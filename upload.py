c380cfb7e4a5f6fed9ba7a8d1fe8e09be22e6ec1
Jayden Kim <jaykim1133@gmail.com>
2025-06-02 18:42:34 -0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,1 +1,31 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os
 
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
+def upload_video(filename, title, description="", categoryId="22", privacyStatus="public"):
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+    credentials = flow.run_console()
+    youtube = build("youtube", "v3", credentials=credentials)
+
+    request_body = {
+        "snippet": {
+            "title": title,
+            "description": description,
+            "categoryId": categoryId,
+        },
+        "status": {
+            "privacyStatus": privacyStatus
+        }
+    }
+
+    media = MediaFileUpload(filename, chunksize=-1, resumable=True)
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body=request_body,
+        media_body=media
+    )
+    response = request.execute()
+    print("Video uploaded.", response)
